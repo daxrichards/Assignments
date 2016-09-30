042a32d37b3f74666103183566d2b20d87333543
Dax Richards <daxrichards29@yahoo.com>
2016-09-30 15:48:26 -0400

    Submit Day35 FE

---
+++ Day35/FE/StudentsList.js
@@ -0,0 +1,75 @@
+
+$(document).ready(function(){
+
+//var token = getSlackToken();
+token = "xoxp-63743851541-69469939616-84593080547-107104a53c95d6eab4c55a0808ca2dd2";
+
+
+
+
+   
+	$.ajax("https://slack.com/api/channels.list?",{
+		
+			method: "POST",
+			data: {
+				token: token
+			}
+			
+
+}).then(function(result) {
+  console.log(result);
+  
+  
+  
+  
+	 for (var i=0; i < result.channels.length; i++) {
+      // name is result[i].name
+      var item = $("<tr><td><input type=\"radio\" class=\"radio2\" name=\"chatroom\" value=\"" + result.channels[i].name + "\">" + result.channels[i].name + "<br>" + "</td></tr>");
+
+        
+      $("#channels").append( item );
+	  
+	 }
+	 
+	
+	
+	 
+	 $("button").click(function(){
+		  
+		             
+		 var radio = $("input[name=chatroom]:checked").val();
+		
+		var msg = $("#textbox").val();
+		
+		  $.ajax("https://slack.com/api/chat.postMessage?", {
+			
+			method: "POST" ,
+			data:{
+				token: token,
+				channel: radio,
+				text: msg,
+				username: "Message Sent",
+				icon_emoji: ":robot_face:"
+			}
+			
+
+			
+			}).then(function(result) {
+			console.log(result);
+			$("#textbox").val("");
+		    $("img").addClass("spin");
+			
+	 		 
+	  });
+ 
+	 });
+ 
+});
+
+});
+
+
+
+
+
+
